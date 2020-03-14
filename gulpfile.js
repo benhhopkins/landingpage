e02f67244acacd47749697c8666b0881059aadcc
Ben Hopkins <ben@bhopkins.net>
2020-03-14 15:08:28 -0500

    Fix gulpfile live reloading, styles and text changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,63 @@
-var gulp = require('gulp'),
-    connect = require('gulp-connect');
-const markdown = require('markdown-it')({
-    html: true
-});
-const markdownattrs = require('markdown-it-attrs')
-const rename = require('gulp-rename');
-const fs = require('fs');
-const map = require('map-stream');
+const { series, parallel, src, dest, watch } = require('gulp');
+const connect = require('gulp-connect');
 const sass = require('gulp-sass');
 
-markdown.use(markdownattrs);
-
-gulp.task('sass', function () {
-    return gulp.src('css/*.scss')
+function sassTask(done) {
+    src('css/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('css'));
-});
+        .pipe(dest('css'));
+    done();
+}
 
-gulp.task('connect', function () {
+function serveTask(done) {
     connect.server({
         root: './',
-        livereload: true
-    });
-});
+        livereload: true,
+        port: 8000,
+    }, function () { this.server.on('close', done) })
+}
 
-gulp.task('watch', function () {
-    gulp.watch('css/*.scss').on('change', gulp.series('sass'));
-});
+function reloadTask(done) {
+    src('./*.html').pipe(connect.reload());
+    done();
+}
 
-gulp.task('default', gulp.series(['sass', 'connect', 'watch']));
+function watchTask(done) {
+    watch('./*.html').on('change', (filepath) =>
+        src(filepath, { read: false }).pipe(connect.reload()));
+    watch('css/*.scss', series(sassTask, reloadTask));
+    done();
+}
+
+module.exports.default = series(sassTask, parallel(serveTask, watchTask));
+
+// gulp.task('sass', function () {
+//     return gulp.src('css/*.scss')
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(gulp.dest('css'));
+// });
+
+// gulp.task('connect', function (done) {
+//     connect.server({
+//         root: './',
+//         livereload: true
+//     }, function () { this.server.on('close', done) });
+// });
+
+// function serveTask(done) {
+//     connect.server({
+//         root: 'public',
+//         livereload: true,
+//         port: 8000,
+//     }, function () { this.server.on('close', done) })
+// }
+
+// gulp.task('watch', function () {
+//     gulp.watch('css/*.scss').on('change', gulp.series('sass'));
+//     gulp.watch('*.html').on('change', gulp.series('default'));
+// });
+
+// gulp.task('default', gulp.series(['sass', 'connect', 'watch']));
 
 // gulp.task('default', gulp.series(['sass'], function () {
 //     gulp.watch('css/*.scss').on('change', gulp.series('sass'));
